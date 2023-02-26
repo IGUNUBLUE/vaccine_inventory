a5da3ea8bb8d39d34029e2f92804a0ebbc6dec2d
Lenin Garizabalo <lcamargo@protonmail.com>
2023-02-25 19:35:53 -0500

    fix(employees): refetch data every render

--- src/hooks/employee/useAllEmployees.js
+++ src/hooks/employee/useAllEmployees.js
@@ -5,38 +5,42 @@ import useSetEmployeesState from '../common/useSetEmployeesState';
 
 export default function useAllEmployees() {
   const { createAlert, finishAlert } = useAlert();
-  const { saveEmployees } = useSetEmployeesState();
+  const { saveEmployees, employees } = useSetEmployeesState();
   const [fullError, setFullError] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
-    getAllEmployees()
-      .then(({ data, error }) => {
-        if (error) {
-          createAlert({
-            show: true,
-            severity: 'error',
-            message: 'Something went wrong...'
-          });
-          return setFullError({ ...error });
-        }
 
-        if (!data.length) {
-          createAlert({
-            show: true,
-            severity: 'info',
-            message: 'There are not employees...'
-          });
-          return setFullError(error);
-        }
+    if (!employees?.length) {
+      getAllEmployees()
+        .then(({ data, error }) => {
+          if (error) {
+            createAlert({
+              show: true,
+              severity: 'error',
+              message: 'Something went wrong...'
+            });
+            return setFullError({ ...error });
+          }
 
-        return saveEmployees(data);
-      })
-      .finally(() => {
-        finishAlert();
-        setIsLoading(false);
-      });
+          if (!data.length) {
+            createAlert({
+              show: true,
+              severity: 'info',
+              message: 'There are not employees...'
+            });
+            return setFullError(error);
+          }
+
+          return saveEmployees(data);
+        })
+        .finally(() => {
+          finishAlert();
+        });
+    }
+
+    setIsLoading(false);
   }, []);
 
   return { isLoading, fullError };
